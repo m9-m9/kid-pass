4914ec782d7fb74c542b2d414221062708e96d6f
정민규 <jmg2165@naver.com>
2024-10-21 22:16:38 +0900

    main 브랜치와 병합 후 stash 코드 재적용

--- src/pages/note/App.tsx
+++ src/pages/note/App.tsx
@@ -1,11 +1,24 @@
+import Container from "../../elements/container/Container";
 import { Label } from "../../elements/label/Label";
 
 const Note = () => {
     return (
         <>
-            <div className="horizonFlexbox gap-8">
-                <Label text="김아기" css="" />
+            <div className="horizonFlexbox gap-8 mb-16">
+                <Label text="김아기" css="babyName" />
+                <Label text="12주" css="babyAge" />
             </div>
+            <Container className="dueDate mb-16">
+                <Label text="결핵" css="dueDate" />
+                <div className="vertivalFlexbox gap-8">
+                    <Label text="D-DAY + 5" css="dueDate" />
+                    <Label text="접종일이 지났습니다" css="dueDate" />
+                </div>
+            </Container>
+            <Container className="vaccination">
+                <Label text="B형 간염 3차" css="vaccination" />
+                <Label text="D-Day" css="vaccination" />
+            </Container>
         </>
     );
 };
