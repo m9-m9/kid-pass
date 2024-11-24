804c65c91e4e308e140bc4b56cbd37fdada3bc92
bongsub kim <96857444+Bongseop-Kim@users.noreply.github.com>
2024-11-24 23:44:37 +0900

    conver to NextJs

--- src/elements/container/Container.tsx
+++ src/elements/container/Container.tsx
@@ -2,24 +2,23 @@ import React from "react";
 import styles from "./container.module.css";
 
 export interface ContainerProps {
-    children: React.ReactNode;
-    className: string;
-    backgroundColor?: string; 
-    
+  children: React.ReactNode;
+  className?: string;
+  backgroundColor?: string;
 }
 
 const Container: React.FC<ContainerProps> = ({ children, className, backgroundColor }) => {
-    // 클래스명 조합
-    const classNames = className
-        .split(" ")
-        .map((cls) => styles[cls] || cls) 
-        .join(" ");
+  // 클래스명 조합
+  const classNames = className
+    ?.split(" ")
+    .map((cls) => styles[cls] || cls)
+    .join(" ");
 
-    return (
-        <div className={classNames} style={{ backgroundColor }}>
-            {children}
-        </div>
-    );
+  return (
+    <div className={classNames} style={{ backgroundColor }}>
+      {children}
+    </div>
+  );
 };
 
 export default Container;
