41fe18f1388888c6fe0b59bc24aff12adc7a9a05
정민규 <jmg2165@naver.com>
2024-11-26 23:08:59 +0900

    design : chapter3 퍼블리싱 완료

--- src/app/profile/chapter/Chapter3.tsx
+++ src/app/profile/chapter/Chapter3.tsx
@@ -4,16 +4,37 @@ import React from "react";
 import { Label } from "@/elements/label/Label";
 import Button from "@/elements/button/Button";
 import { ChapterProps } from "@/hook/useChapter";
+import styles from "../profile.module.css";
 
 const Chapter3: React.FC<ChapterProps> = ({ onNext }) => {
     return (
         <div>
             <Label
                 css="profileLabel"
-                text="김아기의 과거 증상과
-진료 기룍을 알려주세요"
+                text="김아기의 과거 증상과<br>
+진료 기록을 알려주세요"
             />
-            <Button css="nextBtn" onClick={onNext} label="다음"></Button>
+
+            <div className="mt-48">
+                <Label
+                    text="과거에 진단받은 질환이 있나요?"
+                    css="profileDesc"
+                />
+                <div className={`${styles.profileContainer} mt-12`}>
+                    <div
+                        className={styles.profileContainer_item}
+                        onClick={onNext}
+                    >
+                        신생아
+                    </div>
+                    <div
+                        className={styles.profileContainer_item}
+                        onClick={onNext}
+                    >
+                        육아
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
