b743ab0a3617a855078016616503ba1e5993bc8d
정민규 <jmg2165@naver.com>
2024-11-26 23:08:59 +0900

    design : chapter3 퍼블리싱 완료

--- src/app/profile/chapter/Chapter1.tsx
+++ src/app/profile/chapter/Chapter1.tsx
@@ -7,52 +7,37 @@ import { ChapterProps } from "@/hook/useChapter";
 import styles from "../profile.module.css";
 
 const Chapter1: React.FC<ChapterProps> = ({ onNext }) => {
-    const setAge = useProfileStore((state) => state.setAge);
+  const setAge = useProfileStore((state) => state.setAge);
 
-    const handleAgeSelection = (event: React.MouseEvent<HTMLDivElement>) => {
-        const selectedAge = (event.target as HTMLElement).innerHTML.trim(); // innerHTML 가져오기
-        setAge(selectedAge);
-        onNext();
-    };
+  const handleAgeSelection = (event: React.MouseEvent<HTMLDivElement>) => {
+    const selectedAge = (event.target as HTMLElement).innerHTML.trim(); // innerHTML 가져오기
+    setAge(selectedAge);
+    onNext();
+  };
 
-    return (
-        <div>
-            <Label
-                css="profileLabel"
-                text="아이의 나이대가<br>어떻게 되나요?"
-            />
-            <div className={`${styles.profileContainer} mt-48`}>
-                <div
-                    className={styles.profileContainer_item}
-                    onClick={handleAgeSelection}
-                >
-                    신생아
-                </div>
-                <div
-                    className={styles.profileContainer_item}
-                    onClick={handleAgeSelection}
-                >
-                    육아
-                </div>
-                <div
-                    className={styles.profileContainer_item}
-                    onClick={handleAgeSelection}
-                >
-                    초등
-                    <br />
-                    저학년
-                </div>
-                <div
-                    className={styles.profileContainer_item}
-                    onClick={handleAgeSelection}
-                >
-                    초등
-                    <br />
-                    고학년
-                </div>
-            </div>
+  return (
+    <div>
+      <Label css="profileLabel" text="아이의 나이대가<br>어떻게 되나요?" />
+      <div className={`${styles.profileContainer} mt-48`}>
+        <div className={styles.profileContainer_item} onClick={handleAgeSelection}>
+          신생아
         </div>
-    );
+        <div className={styles.profileContainer_item} onClick={handleAgeSelection}>
+          육아
+        </div>
+        <div className={styles.profileContainer_item} onClick={handleAgeSelection}>
+          초등
+          <br />
+          저학년
+        </div>
+        <div className={styles.profileContainer_item} onClick={handleAgeSelection}>
+          초등
+          <br />
+          고학년
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chapter1;
--- src/app/profile/chapter/Chapter3.tsx
+++ src/app/profile/chapter/Chapter3.tsx
@@ -6,36 +6,27 @@ import { ChapterProps } from "@/hook/useChapter";
 import styles from "../profile.module.css";
 
 const Chapter3: React.FC<ChapterProps> = ({ goToChapter }) => {
-    return (
-        <div>
-            <Label
-                css="profileLabel"
-                text="김아기의 과거 증상과<br>
+  return (
+    <div>
+      <Label
+        css="profileLabel"
+        text="김아기의 과거 증상과<br>
 진료 기록을 알려주세요"
-            />
+      />
 
-            <div className="mt-48">
-                <Label
-                    text="과거에 진단받은 질환이 있나요?"
-                    css="profileDesc"
-                />
-                <div className={`${styles.profileContainer} mt-12`}>
-                    <div
-                        className={styles.profileContainer_item}
-                        onClick={() => goToChapter(4)}
-                    >
-                        있어요
-                    </div>
-                    <div
-                        className={styles.profileContainer_item}
-                        onClick={() => goToChapter(5)}
-                    >
-                        없어요
-                    </div>
-                </div>
-            </div>
+      <div className="mt-48">
+        <Label text="과거에 진단받은 질환이 있나요?" css="profileDesc" />
+        <div className={`${styles.profileContainer} mt-12`}>
+          <div className={styles.profileContainer_item} onClick={() => goToChapter(4)}>
+            있어요
+          </div>
+          <div className={styles.profileContainer_item} onClick={() => goToChapter(5)}>
+            없어요
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Chapter3;
