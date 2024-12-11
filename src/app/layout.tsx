ef7244d1873b275cab4f42b6a6361d0718eb5c12
bongsub kim <bong989971@gmail.com>
2024-12-11 22:40:33 +0900

    Bottom Modal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,36 @@
 import type { Metadata } from "next";
 import "@/styles/global.css";
 import localFont from "next/font/local";
-import ProfileModal from "@/components/modal/Modal";
-import Container from "@/elements/container/Container";
+import CustomeModal from "@/components/modal/Modal";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+  src: [
+    {
+      path: "./fonts/Pretendard-Thin.woff",
+      weight: "100",
+    },
+    {
+      path: "./fonts/Pretendard-Light.woff",
+      weight: "300",
+    },
+    {
+      path: "./fonts/Pretendard-Regular.woff",
+      weight: "400",
+    },
+    {
+      path: "./fonts/Pretendard-Medium.woff",
+      weight: "500",
+    },
+    {
+      path: "./fonts/Pretendard-Bold.woff",
+      weight: "700",
+    },
+    {
+      path: "./fonts/Pretendard-Black.woff",
+      weight: "900",
+    },
+  ],
+  variable: "--font-weight-regular",
 });
 
 export const metadata: Metadata = {
@@ -26,10 +44,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en" className={`${geistSans.variable} `}>
       <body>
         {children}
-        <ProfileModal />
+        <CustomeModal />
       </body>
     </html>
   );
--- src/app/profile/chapter/Chapter1.tsx
+++ src/app/profile/chapter/Chapter1.tsx
@@ -6,6 +6,7 @@ import { Label } from "@/elements/label/Label";
 import { ChapterProps } from "@/hook/useChapter";
 import styles from "../profile.module.css";
 import Grid from "@/elements/grid/Grid";
+import Spacer from "@/elements/spacer/Spacer";
 
 const Chapter1: React.FC<ChapterProps> = ({ onNext }) => {
   const setAge = useProfileStore((state) => state.setAge);
@@ -27,6 +28,7 @@ const Chapter1: React.FC<ChapterProps> = ({ onNext }) => {
   return (
     <div>
       <Label css="profileLabel" text="아이의 나이대가<br>어떻게 되나요?" />
+      <Spacer height={56} />
       <Grid
         items={[
           <Item txt={"신생아"} />,
